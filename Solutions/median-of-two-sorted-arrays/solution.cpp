062dfc01bf50402f4f48d0e76c41407ac768a9bd
Tanmaychandekar45 <165395954+Tanmaychandekar45@users.noreply.github.com>
2026-07-08 14:12:29 +0530

    Sync C++ solution for LeetCode: Median of Two Sorted Arrays

--- Solutions/median-of-two-sorted-arrays/solution.cpp
+++ Solutions/median-of-two-sorted-arrays/solution.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
-        vector<int>ans;
-
-        for(int i=0;i<nums.size();i++){
-            int first=nums[i];
-            int second=target-first;
-
-            if(m.find(second) != m.end()){
-                ans.push_back(i);
-                ans.push_back(m[second]);
-                break;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int i=0;
+        int j=0;
+        vector<int>res;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
             }
-            m[first]=i;
+            else{
+                res.push_back(nums2[j]);
+                j++;
+            }
         }
-        return ans;
+        while(i<nums1.size()){
+            res.push_back(nums1[i]);
+            i++;
+        }
+        while(j<nums2.size()){
+            res.push_back(nums2[j]);
+            j++;
+        }
+
+        int mid=res.size()/2;
+
+        if(res.size()%2 != 0){
+            return res[mid];
+        }
+        else {
+            return (res[mid-1]+res[mid])/2.0;
+        }
     }
 };
